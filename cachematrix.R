0b4da97a5f110896eb20d9a73ce8d912fc54ee09
tempgitab <bardespam@gmail.com>
2014-07-27 17:30:05 +0200

    implemented makeCacheMatrix, cacheSolve and matequal functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The following functions allow to chache the inverse of a matrix.
+##This, allows to instantly return the inverse when needed and avoids a computation overhead
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##This function builds a matrix "class" that allows to store a cached copy of the inverse
+makeCacheMatrix <- function(savedMatrix = matrix()) {
+    cachedInv <- NULL
+    set <- function(y) {
+        #I replace the saved matrix only if the new one is different from the previous one
+        if(!matequal(savedMatrix,y))
+        {
+            savedMatrix <<- y
+            cachedInv <<- NULL
+        }
+    }
+    get <- function() savedMatrix
+    setInv <- function(inverse) cachedInv <<- inverse
+    getInv <- function() cachedInv
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)    
 }
 
 
-## Write a short comment describing this function
-
+## This function returns a matrix that is the inverse of 'x'
+## If already computed before it returns the cached version
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    cachedInv <- x$getInv()
+    if(!is.null(cachedInv)) {
+        message("getting cached data")
+        return(cachedInv)
+    }
+    savedMatrix <- x$get()
+    cachedInv <- solve(savedMatrix, ...)
+    x$setInv(cachedInv)
+    cachedInv
+
 }
+
+# this function compare two matrices and check if they are identical
+matequal <- function(x, y)
+    {
+        is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
+    } 
