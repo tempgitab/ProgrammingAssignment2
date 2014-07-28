56fdea636ff919819bc56db9324576fccc27f0a7
tempgitab <bardespam@gmail.com>
2014-07-28 15:16:03 +0200

    reshaping and enhancement of class makeCacheMatrix
    #----------------------------------------------------------------------------
    #NOTE FOR PEER-REVIEWERS: if you are reading this you are looking at a different
    # commit with respect to that submitted. This an enhanced versions that fixes some 
    # design inefficiences present in the suggested starting script. 
    # As a matter of fact, this way the cacheSolve function is no longer needed and all is encapsulated
    # within the makeCacheMatrix.
    # You should evaluate the other one but any comment or suggestion on this version is welcome :)
    #----------------------------------------------------------------------------

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
+#----------------------------------------------------------------------------
+#NOTE FOR PEER-REVIEWERS: if you are reading this you are looking at a different
+# commit with respect to that submitted. This an enhanced versions that fixes some 
+# design inefficiences present in the suggested starting script. 
+# As a matter of fact, this way the cacheSolve function is no longer needed and all is encapsulated
+# within the makeCacheMatrix.
+# You should evaluate the other one but any comment or suggestion on this version is welcome :)
+#----------------------------------------------------------------------------
+
 ##The following functions allow to chache the inverse of a matrix.
 ##This, allows to instantly return the inverse when needed and avoids a computation overhead
 
@@ -5,6 +14,7 @@
 makeCacheMatrix <- function(savedMatrix = matrix()) 
 {
     cachedInv <- NULL
+    #function that (re)sets the matrix
     set <- function(y) 
     {
         #I replace the saved matrix only if the new one is different from the previous one
@@ -14,33 +24,55 @@ makeCacheMatrix <- function(savedMatrix = matrix())
             cachedInv <<- NULL
         }
     }
-    get <- function() savedMatrix
-    setInv <- function(inverse) cachedInv <<- inverse
-    getInv <- function() cachedInv
-    list(set = set, get = get,
-         setInv = setInv,
-         getInv = getInv)    
+    #function that returns the saved matrix
+    get <- function() return(savedMatrix)
+    
+    #function that sets the inverse. 
+    #If called without args the inverse is computed from scratch
+    setInv <- function(inverse,...) 
+    {
+        if(missing(inverse))
+        {
+            cachedInv <<- solve(savedMatrix, ...)
+        }
+        else 
+        {
+            cachedInv <<- inverse
+        }
+    }
+    #function that returns the inverse either cached or computed from scratch
+    getInv <- function(...) 
+    {
+        if(!is.null(cachedInv))
+        {
+            message("getting cached data")
+            return(cachedInv)
+        }
+        else
+        {
+            message("computing the inverse")
+            setInv()
+            return(cachedInv)
+        }        
+    }
+   
+    # auxiliary functions: 
+    # this function compare two matrices and check if they are identical
+    matequal <- function(x, y)
+    {
+        return(is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y))
+    }     
+    
+    # aggregate and return visible functions
+    return(list(set = set, get = get,
+                setInv = setInv,
+                getInv = getInv))   
 }
-
 
 ## This function returns a matrix that is the inverse of 'x'
 ## If already computed before it returns the cached version
+## with the modification made to the makeCacheMatrix class this function is useless...but kept for backward compatibility
 cacheSolve <- function(x, ...) 
-{
-    cachedInv <- x$getInv()
-    if(!is.null(cachedInv)) 
-    {
-        message("getting cached data")
-        return(cachedInv)
-    }
-    savedMatrix <- x$get()
-    cachedInv <- solve(savedMatrix, ...)
-    x$setInv(cachedInv)
-    cachedInv
+{ 
+    return(x$getInv())
 }
-
-# this function compare two matrices and check if they are identical
-matequal <- function(x, y)
-{
-    is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
-} 
